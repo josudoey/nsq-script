1f86dbb95f38cb05e4c1b1e740f828ce50aa7874
Joey Fang <josudoey@gmail.com>
2017-03-23 17:40:23 +0800

    [modify] patch install script

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -4,4 +4,9 @@ go_version=1.6.2
 cd /tmp
 wget https://s3.amazonaws.com/bitly-downloads/nsq/nsq-${version}.linux-amd64.go${go_version}.tar.gz -O nsq-${version}.linux-amd64.go${go_version}.tar.gz
 tar -zxvf nsq-${version}.linux-amd64.go${go_version}.tar.gz
-install nsq-${version}.linux-amd64.go${go_version}/bin/nsqd /usr/local/bin/
+
+
+for file in nsqadmin  nsqd  nsqlookupd  nsq_pubsub  nsq_stat  nsq_tail  nsq_to_file  nsq_to_http  nsq_to_nsq  to_nsq;
+do
+  install nsq-${version}.linux-amd64.go${go_version}/bin/${file} /usr/local/bin/
+done
